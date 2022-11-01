d763c3663955d3abca443a7638509e6989b3daa5
Robert <valnerrobert@gmail.com>
2022-11-01 20:00:45 +0200

    Ran clang-format on the codebase

--- src/dummy_pub_srv.cpp
+++ src/dummy_pub_srv.cpp
@@ -1,36 +1,36 @@
 /**
  * @file dummy_pub_srv.cpp
  * @author Robert Valner
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-11-01
- * 
+ *
  * @copyright Copyright 2022 Robert Valner
- * 
+ *
  */
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "ros_ci_test/SetDuration.h"
-#include <mutex> // NOLINT [build/c++11]
+#include <mutex>  // NOLINT [build/c++11]
 
-double sleep_duration{1.0};
+double sleep_duration{ 1.0 };
 std::mutex sleep_duration_mutex;
 
 /**
  * @brief Service callback for setting the sleep duration
- * 
- * @param req 
- * @param res 
- * @return true 
- * @return false 
+ *
+ * @param req
+ * @param res
+ * @return true
+ * @return false
  */
 bool serviceCb(ros_ci_test::SetDuration::Request& req, ros_ci_test::SetDuration::Response& res)
 {
   std::lock_guard<std::mutex> l(sleep_duration_mutex);
   sleep_duration = req.duration;
-   res.duration = sleep_duration;
-   ROS_INFO_STREAM("set sleep duration to: " << sleep_duration << " s");
+  res.duration = sleep_duration;
+  ROS_INFO_STREAM("set sleep duration to: " << sleep_duration << " s");
   return true;
 }
 
@@ -54,7 +54,7 @@ int main(int argc, char** argv)
 
   while (ros::ok())
   {
-    static unsigned int i{0};
+    static unsigned int i{ 0 };
     string_msg.data = "Hi, " + std::to_string(i++);
     ROS_INFO_STREAM("sending: " << string_msg.data);
     pub.publish(string_msg);
--- src/test/dynamic_duration_test.cpp
+++ src/test/dynamic_duration_test.cpp
@@ -1,12 +1,12 @@
 /**
  * @file dynamic_duration_test.cpp
  * @author your name (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-11-01
- * 
+ *
  * @copyright Copyright 2022 Robert Valner
- * 
+ *
  */
 
 #include <ros/ros.h>
@@ -14,8 +14,8 @@
 #include "ros_ci_test/SetDuration.h"
 #include "std_msgs/String.h"
 
-double avg_duration{0};
-double new_duration{0.05};
+double avg_duration{ 0 };
+double new_duration{ 0.05 };
 double rate_before;
 ros::Subscriber rate_sub;
 
@@ -28,7 +28,7 @@ void stringMsgCb(const std_msgs::String& msg)
   static auto previous_time = current_time;
 
   ros::Duration dt = current_time - previous_time;
-  avg_duration = 0.7*avg_duration + 0.3*dt.toSec();
+  avg_duration = 0.7 * avg_duration + 0.3 * dt.toSec();
 
   previous_time = current_time;
 }
@@ -40,7 +40,7 @@ TEST(TestSuite, service_call_test)
 {
   // First, measure current publishing rate
   ros::Duration(4).sleep();
-  rate_before = 1.0/avg_duration;
+  rate_before = 1.0 / avg_duration;
 
   // Set up the service client for invoking the set_duration service
   ros::NodeHandle nh;
@@ -58,8 +58,7 @@ TEST(TestSuite, service_call_test)
   {
     // Check if the response is equal to the expected result
     EXPECT_EQ(srv_msg.response.duration, expected_result)
-      << "Expected " << expected_result
-      << " but got " << srv_msg.response.duration;
+        << "Expected " << expected_result << " but got " << srv_msg.response.duration;
   }
   else
   {
@@ -75,22 +74,20 @@ TEST(TestSuite, dynamic_rate_test)
 {
   // Measure current publishing rate
   ros::Duration(4).sleep();
-  double rate_after = 1.0/avg_duration;
-  double expected_rate = 1.0/new_duration;
+  double rate_after = 1.0 / avg_duration;
+  double expected_rate = 1.0 / new_duration;
 
-  std::cerr << "rate_before: " << rate_before
-    << " Hz, expected_rate: " << expected_rate
-    << " Hz, current_rate: " << rate_after << std::endl;
+  std::cerr << "rate_before: " << rate_before << " Hz, expected_rate: " << expected_rate
+            << " Hz, current_rate: " << rate_after << std::endl;
 
   EXPECT_TRUE((rate_after <= expected_rate + 1) || (rate_after >= expected_rate - 1))
-    << "Expected " << expected_rate
-    << " Hz but got " << rate_after << " Hz";
+      << "Expected " << expected_rate << " Hz but got " << rate_after << " Hz";
 }
 
 /*
  * Main
  */
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
   // Initialize the gtes
   testing::InitGoogleTest(&argc, argv);
